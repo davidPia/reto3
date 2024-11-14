bf1263fce509a3ac2836329623add452e7a5db38
mariopellicer <marioopelli23@gmail.com>
2024-11-14 09:37:45 +0100

    Funciones2 terminadas

--- Reto3/src/retos/Funciones2.java
+++ Reto3/src/retos/Funciones2.java
@@ -19,7 +19,22 @@ public class Funciones2 {
 		} while (Fecha1.isBefore(Fecha2.withDayOfMonth(1)));
 		double MediaDias=0;
 		MediaDias=TotalDias/ContadorDias;
-		
+		System.out.println("la media de los dias es:"+MediaDias);
+		Consonantes(Fecha2);
 	}
-	
+	public static void Consonantes(LocalDate Fecha2) {
+		String Mes = String.valueOf(Fecha2.getMonth());
+		Mes=Mes.toLowerCase();
+		char letra;
+		for (int i = 0; i < Mes.length(); i++) {
+			letra=Mes.charAt(i);
+			if (letra!='a' && letra!='e' && letra!='i' && letra!='o' && letra!='u') {
+				if (i==0) {
+					System.out.println(letra);
+				}else {
+					System.out.println(","+letra);
+				}
+			}
+		}
+	}
 }
