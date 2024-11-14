3419870846ae3c71f87c31841dcd20e9785a4031
mariopellicer <marioopelli23@gmail.com>
2024-11-14 08:29:26 +0100

    añadir clase funciones y parte del main

--- Reto3/src/retos/Reto3.java
+++ Reto3/src/retos/Reto3.java
@@ -1,10 +1,20 @@
 package retos;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Reto3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		LocalDate Fecha1 = Funciones.dimeFecha(sc, "introduce la primera fecha (dd/MM/yyyy)");
+		LocalDate Fecha2 =null;
+		do {
+			Fecha2 = Funciones.dimeFecha(sc, "introduce la segunda fecha (dd/MM/yyyy)");
+			if (!(Fecha2.isAfter(Fecha1))) {
+				System.out.println("la segunda fecha debe ser posterior a la primera");
+			}
+		} while (!Fecha2.isAfter(Fecha1));
 	}
 
 }
